4dab0e0a8e2e6bd2d86fc59f248c3f9393c6912e
Đào Như Triệu <daonhutrieu15052003>
2024-09-14 23:16:14 +0700

    Cập nhật database

--- lib/initdatabase.dart
+++ lib/initdatabase.dart
@@ -1,13 +1,13 @@
-import 'package:lap_english/Person.dart';
+import 'package:lap_english/Student.dart';
 import 'package:lap_english/vip_sqflite/Database.dart';
 
 ///===  Hàm khởi tạo database    ===
 DatabaseApp initdatabase() {
   return DatabaseApp(
       name: 'data',
-      version: 4,
+      version: 6,
       tableSchemas: [
-        PersonTable(),
+        StudentTable(),
       ]
   );
 }
